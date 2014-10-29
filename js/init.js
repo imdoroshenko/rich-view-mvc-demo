72bde30e2bb113c917fc1be77619968350a02ce0
Ivan Doroshenko <idoroshenko@blackice.local>
2014-10-29 02:49:04 +0200

    comments removed

--- js/init.js
+++ js/init.js
@@ -3,7 +3,4 @@ window.addEventListener('load', function () {
     RV.setContainer(document.body);
     rg.getInstance('UsersController');
     rg.getInjection('RV_routerEM').trigger('users-page');
-    //new UsersController();
-   // RV.routerNS.getEventManager().trigger('users-page');
-    //new RV.Router().init();
 });
